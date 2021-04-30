85193c9697d20710d021f1e184ae6808f263ea70
andrasputyera <andras.putyera@gmail.com>
2021-04-30 12:32:23 +0200

    set up firestore queries for retrieving records

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit a Smoothie {{ this.$route.params.smoothie_slug }}</h2>
+    <div v-if="smoothie" class="edit-smoothie container">
+        <h2>Edit {{ smoothie.title }} Smoothie</h2>
     </div>
 </template>
 
 <script>
+import db from '@/firebase/config'
+
 export default {
     name: 'EditSmoothie',
     data() {
-        return{
+        return {
+            // Storage for data record that we retrieve from database
+            smoothie: null
     }
+  },
+  created() {
+      let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+      
+      ref.get().then(snapshot => {
+          snapshot.forEach(doc => {
+              this.smoothie = doc.data()
+              this.smoothie.id = doc.id
+          })
+      })
   }
 }
 </script>
